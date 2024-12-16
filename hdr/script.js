aeff2fc8a4ba426c5dad951dd98320ebdd593393
Gabriel Garcia <garciat@uber.com>
2024-12-16 13:34:05 +0100

    go back to mvp matrix

--- hdr/script.js
+++ hdr/script.js
@@ -11,7 +11,7 @@ function makeVertex([x, y], [r, g, b, a]) {
 }
 
 // Vertex data for triangle
-const vertexDataSize = 4*4 + 4*4; // position + color
+const vertexDataSize = 4 * 4 + 4 * 4; // position + color
 const vertices = new Float32Array([
   ...makeVertex([0, 0.6], [1, 1, 1, 1]),
   ...makeVertex([-0.5, -0.6], [1, 1, 1, 1]),
@@ -20,15 +20,23 @@ const vertices = new Float32Array([
 const vertexCount = vertices.byteLength / vertexDataSize;
 
 function makeInstance(offset, scale, rotDeg, tint) {
+  const model = mat4.identity();
+  mat4.translate(model, vec3.fromValues(offset[0], offset[1], 0), model);
+  mat4.scale(model, vec3.fromValues(scale[0], scale[1], 1), model);
+  mat4.rotateZ(model, rotDeg / 360 * Math.PI * 2, model);
+
+  const mvp = mat4.create();
+  const mvp_inv = mat4.create();
+
   return [
-    ...[offset[0], offset[1], 0],
-    ...[scale[0], scale[1], 0],
-    ...[0, 0, rotDeg/360*Math.PI*2],
     ...tint,
+    ...model,
+    ...mvp,
+    ...mvp_inv,
   ];
 }
 
-const instanceSize = 3*4 + 3*4 + 3*4 + 4*4; // translation + scale + rotation + tint
+const instanceSize = 4*4 + 4*16 + 4*16 + 4*16; // tint + model + mvp + mvp_inv
 const instances = new Float32Array([
   ...makeInstance([0, 0], [1, 1], 0, [1, 1, 1, 1]),
   ...makeInstance([0.5, 0.5], [0.5, 0.5], 0, [1, 0, 0, 1]),
@@ -37,6 +45,16 @@ const instances = new Float32Array([
   ...makeInstance([-1, 0], [0.5, 0.5], 90, [1, 1, 0, 1]),
 ]);
 const instanceCount = instances.byteLength / instanceSize;
+
+function getInstanceParts(i) {
+  const instance = instances.subarray(i * instanceSize / 4, (i + 1) * instanceSize / 4);
+  const tint = instance.subarray(0, 4);
+  const model = instance.subarray(4, 20);
+  const mvp = instance.subarray(20, 36);
+  const mvp_inv = instance.subarray(36, 52);
+
+  return { tint, model, mvp, mvp_inv };
+}
 
 // Main function
 async function init() {
@@ -121,24 +139,48 @@ async function init() {
     {
       attributes: [
         {
-          shaderLocation: 2, // translation
+          shaderLocation: 2, // tint
           offset: 0,
-          format: 'float32x3',
-
+          format: 'float32x4',
         },
         {
-          shaderLocation: 3, // scale
-          offset: 3 * 4,
-          format: 'float32x3',
+          shaderLocation: 3, // mvpMatrix0
+          offset: 4 * 16 + 4 * 4,
+          format: 'float32x4',
         },
         {
-          shaderLocation: 4, // rotation
-          offset: 3 * 4 + 3 * 4,
-          format: 'float32x3',
+          shaderLocation: 4, // mvpMatrix1
+          offset: 4 * 16 + 4 * 8,
+          format: 'float32x4',
         },
         {
-          shaderLocation: 5, // tint
-          offset: 3 * 4 + 3 * 4 + 3 * 4,
+          shaderLocation: 5, // mvpMatrix2
+          offset: 4 * 16 + 4 * 12,
+          format: 'float32x4',
+        },
+        {
+          shaderLocation: 6, // mvpMatrix3
+          offset: 4 * 16 + 4 * 16,
+          format: 'float32x4',
+        },
+        {
+          shaderLocation: 7, // mvpInvMatrix0
+          offset: 4 * 16 + 4 * 20,
+          format: 'float32x4',
+        },
+        {
+          shaderLocation: 8, // mvpInvMatrix1
+          offset: 4 * 16 + 4 * 24,
+          format: 'float32x4',
+        },
+        {
+          shaderLocation: 9, // mvpInvMatrix2
+          offset: 4 * 16 + 4 * 28,
+          format: 'float32x4',
+        },
+        {
+          shaderLocation: 10, // mvpInvMatrix3
+          offset: 4 * 16 + 4 * 32,
           format: 'float32x4',
         },
       ],
@@ -186,11 +228,11 @@ async function init() {
   const renderPipeline = device.createRenderPipeline(pipelineDescriptor);
 
   // Uniforms
-  const time = new Float32Array(1);
+  const timeUniform = new Float32Array(1);
   const uniforms = new Float32Array(16);
 
   const timeBuffer = device.createBuffer({
-    size: time.byteLength,
+    size: timeUniform.byteLength,
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
   });
 
@@ -218,22 +260,47 @@ async function init() {
   });
 
   const aspect = canvas.width / canvas.height;
-  const projectionMatrix = mat4.perspective((2 * Math.PI) / 5, aspect, 1, 100.0);
-  const modelViewProjectionMatrix = uniforms.subarray(0, 16);
 
-  function updateTransformationMatrix(timestamp) {
-    const viewMatrix = mat4.identity();
-    mat4.translate(viewMatrix, vec3.fromValues(0, 0, -2), viewMatrix);
-    mat4.multiply(projectionMatrix, viewMatrix, modelViewProjectionMatrix);
+  const viewMatrix = mat4.create();
+
+  const projectionMatrix = mat4.perspective((2 * Math.PI) / 5, aspect, 1, 100.0);
+
+  function updateUniforms(time) {
+    mat4.identity(viewMatrix);
+    mat4.translate(viewMatrix, vec3.fromValues(0, 0, -3), viewMatrix);
+    mat4.rotateY(viewMatrix, time, viewMatrix);
+
+    mat4.identity(uniforms);
+    mat4.multiply(uniforms, projectionMatrix, uniforms);
+    mat4.multiply(uniforms, viewMatrix, uniforms);
+  }
+
+  function updateInstances(time) {
+    for (let i = 0; i < instanceCount; i++) {
+      const { tint, model, mvp, mvp_inv } = getInstanceParts(i);
+
+      mat4.identity(mvp);
+      mat4.multiply(mvp, projectionMatrix, mvp);
+      mat4.multiply(mvp, viewMatrix, mvp);
+      mat4.multiply(mvp, model, mvp);
+      mat4.rotateY(mvp, time, mvp);
+
+      mat4.invert(mvp, mvp_inv);
+    }
   }
 
   function frame(timestamp) {
-    // Update uniforms
-    time[0] = timestamp / 1000;
-    device.queue.writeBuffer(timeBuffer, 0, time, 0, time.length);
+    const time = timestamp / 1000;
 
-    updateTransformationMatrix(timestamp);
+    // Update uniforms
+    timeUniform[0] = time;
+    device.queue.writeBuffer(timeBuffer, 0, timeUniform, 0, timeUniform.length);
+
+    updateUniforms(time);
     device.queue.writeBuffer(uniformBuffer, 0, uniforms, 0, uniforms.length);
+
+    updateInstances(time);
+    device.queue.writeBuffer(instanceBuffer, 0, instances, 0, instances.length);
 
     // 7: Create GPUCommandEncoder to issue commands to the GPU
     // Note: render pass descriptor, command encoder, etc. are destroyed after use, fresh one needed for each frame.
--- hdr/shaders.wgsl
+++ hdr/shaders.wgsl
@@ -1,19 +1,44 @@
 struct Uniforms {
-  modelViewProjectionMatrix : mat4x4f,
+  viewProjectionMatrix : mat4x4f,
 }
 @group(0) @binding(0) var<uniform> time : f32;
 @group(0) @binding(1) var<uniform> uniforms : Uniforms;
 
-// Locations: buffers.[*].attributes.[*].shaderLocation
 struct VertexIn {
-  // vertex
   @location(0) position : vec4f,
   @location(1) color : vec4f,
-  // instance
-  @location(2) translation : vec3f,
-  @location(3) scale : vec3f,
-  @location(4) rotation : vec3f,
-  @location(5) tint : vec4f,
+}
+
+struct InstanceIn {
+  @location(2) tint : vec4f,
+
+  @location(3) mvpMatrix0 : vec4f,
+  @location(4) mvpMatrix1 : vec4f,
+  @location(5) mvpMatrix2 : vec4f,
+  @location(6) mvpMatrix3 : vec4f,
+
+  @location(7) mvpInvMatrix0 : vec4f,
+  @location(8) mvpInvMatrix1 : vec4f,
+  @location(9) mvpInvMatrix2 : vec4f,
+  @location(10) mvpInvMatrix3 : vec4f,
+}
+
+fn mvp_matrix(instance: InstanceIn) -> mat4x4f {
+  return mat4x4f(
+    instance.mvpMatrix0,
+    instance.mvpMatrix1,
+    instance.mvpMatrix2,
+    instance.mvpMatrix3,
+  );
+}
+
+fn mvp_inv_matrix(instance: InstanceIn) -> mat4x4f {
+  return mat4x4f(
+    instance.mvpInvMatrix0,
+    instance.mvpInvMatrix1,
+    instance.mvpInvMatrix2,
+    instance.mvpInvMatrix3,
+  );
 }
 
 struct VertexOut {
@@ -26,20 +51,14 @@ struct FragmentOut {
 }
 
 @vertex
-fn vertex_main(input: VertexIn) -> VertexOut
+fn vertex_main(model: VertexIn, instance: InstanceIn) -> VertexOut
 {
+  let _t = time; // keep the compiler happy
+  var _m = uniforms.viewProjectionMatrix;
+
   var output : VertexOut;
-  output.position =
-    uniforms.modelViewProjectionMatrix
-    * (
-      identity4x4()
-      * translation4x4f(input.translation)
-      * rotation4x4f(input.rotation)
-      * rotation4x4f(vec3f(0.0, time, 0.0))
-      * scale4x4f(input.scale)
-      * input.position
-    );
-  output.color = input.color * input.tint;
+  output.position = mvp_matrix(instance) * model.position;
+  output.color = model.color * instance.tint;
   return output;
 }
 
@@ -49,37 +68,4 @@ fn fragment_main(fragData: VertexOut) -> FragmentOut
   var output : FragmentOut;
   output.color = fragData.color;
   return output;
-}
-
-fn rotation4x4f(a: vec3f) -> mat4x4f {
-  var c = vec3f(cos(a.x), cos(a.y), cos(a.z));
-  var s = vec3f(sin(a.x), sin(a.y), sin(a.z));
-  return mat4x4f(
-    vec4f(c.y * c.z, c.y * s.z, -s.y, 0.0),
-    vec4f(s.x * s.y * c.z - c.x * s.z, s.x * s.y * s.z + c.x * c.z, s.x * c.y, 0.0),
-    vec4f(c.x * s.y * c.z + s.x * s.z, c.x * s.y * s.z - s.x * c.z, c.x * c.y, 0.0),
-    vec4f(0.0, 0.0, 0.0, 1.0)
-  );
-}
-
-fn translation4x4f(t: vec3f) -> mat4x4f {
-  return mat4x4f(
-    vec4f(1.0, 0.0, 0.0, 0.0),
-    vec4f(0.0, 1.0, 0.0, 0.0),
-    vec4f(0.0, 0.0, 1.0, 0.0),
-    vec4f(t.x, t.y, t.z, 1.0)
-  );
-}
-
-fn scale4x4f(s: vec3f) -> mat4x4f {
-  return mat4x4f(
-    vec4f(s.x, 0.0, 0.0, 0.0),
-    vec4f(0.0, s.y, 0.0, 0.0),
-    vec4f(0.0, 0.0, s.z, 0.0),
-    vec4f(0.0, 0.0, 0.0, 1.0)
-  );
-}
-
-fn identity4x4() -> mat4x4f {
-  return scale4x4f(vec3f(1.0, 1.0, 1.0));
 }
