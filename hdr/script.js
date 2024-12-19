525a44098bbb3a846603609287e2bc26ee72bf74
Gabriel Garcia <garciat@uber.com>
2024-12-19 16:17:07 +0100

    add floor plane with grass texture

--- hdr/script.js
+++ hdr/script.js
@@ -4,9 +4,6 @@ import {
   mat4,
 } from 'https://wgpu-matrix.org/dist/3.x/wgpu-matrix.module.js';
 
-// Clear color for GPURenderPassDescriptor
-const clearColor = { r: 0.2, g: 0.2, b: 0.2, a: 1.0 };
-
 function makeVertex([x, y, z] = [0, 0, 0], [r, g, b, a] = [1, 1, 1, 1], [u, v] = [0, 0], [nx, ny, nz] = [0, 0, -1]) {
   return [
     ...[x, y, z ?? 0, 1], // position
@@ -15,6 +12,29 @@ function makeVertex([x, y, z] = [0, 0, 0], [r, g, b, a] = [1, 1, 1, 1], [u, v] =
     ...[u, v], // uv
   ];
 }
+
+const vertexDataSize = 4 * makeVertex().length;
+
+function makeInstance([x, y, z] = [0, 0, 0], [sx, sy, sz] = [1, 1, 1], [rx, ry, rz] = [0, 0, 0], tint = [1, 1, 1, 1]) {
+  const model = mat4.identity();
+  mat4.translate(model, vec3.fromValues(x, y, z), model);
+  mat4.scale(model, vec3.fromValues(sx, sy, sz), model);
+  mat4.rotateX(model, rx, model);
+  mat4.rotateY(model, ry, model);
+  mat4.rotateZ(model, rz, model);
+
+  const mvMatrix = mat4.create();
+  const normalMatrix = mat4.create();
+
+  return [
+    ...tint,
+    ...model,
+    ...mvMatrix,
+    ...normalMatrix,
+  ];
+}
+
+const instanceDataSize = 4 * makeInstance().length;
 
 function makeCube() {
   return [
@@ -63,39 +83,53 @@ function makeCube() {
   ];
 }
 
-// Vertex data for triangle
-const vertexDataSize = 4 * makeVertex().length;
-const vertices = new Float32Array([
-  ...makeCube(),
-]);
-const vertexCount = vertices.byteLength / vertexDataSize;
+function makePlane(divisions) {
+  const vertices = [];
+  const step = 1 / divisions;
+  for (let x = 0; x < divisions; x++) {
+    for (let y = 0; y < divisions; y++) {
+      const x0 = x * step - 0.5;
+      const x1 = (x + 1) * step - 0.5;
+      const y0 = y * step - 0.5;
+      const y1 = (y + 1) * step - 0.5;
 
-function makeInstance([x, y, z] = [0, 0, 0], [sx, sy, sz] = [1, 1, 1], rotDeg = 0, tint = [1, 1, 1, 1]) {
-  const model = mat4.identity();
-  mat4.translate(model, vec3.fromValues(x, y, z), model);
-  mat4.scale(model, vec3.fromValues(sx, sy, sz), model);
-  mat4.rotateZ(model, rotDeg / 360 * Math.PI * 2, model);
-
-  const mvMatrix = mat4.create();
-  const normalMatrix = mat4.create();
-
-  return [
-    ...tint,
-    ...model,
-    ...mvMatrix,
-    ...normalMatrix,
-  ];
+      vertices.push(
+        ...makeVertex([x0, y0, 0], [1, 1, 1, 1], [0, 0], [0, 0, 1]),
+        ...makeVertex([x1, y0, 0], [1, 1, 1, 1], [1, 0], [0, 0, 1]),
+        ...makeVertex([x1, y1, 0], [1, 1, 1, 1], [1, 1], [0, 0, 1]),
+        ...makeVertex([x0, y1, 0], [1, 1, 1, 1], [0, 1], [0, 0, 1]),
+        ...makeVertex([x0, y0, 0], [1, 1, 1, 1], [0, 0], [0, 0, 1]),
+        ...makeVertex([x1, y1, 0], [1, 1, 1, 1], [1, 1], [0, 0, 1]),
+      );
+    }
+  }
+  return vertices;
 }
 
-const instanceSize = 4 * makeInstance().length;
-const instances = new Float32Array([
-  ...makeInstance([0, 0, 0], [0.5, 0.5, 0.5], 0, [1, 1, 1, 1]),
-  ...makeInstance([0, 0, -3], [0.05, 0.05, 0.05], 0, [1, 1, 1, 1]), // light
+// Vertex data for triangle
+const cubeVertexData = new Float32Array([
+  ...makeCube(),
 ]);
-const instanceCount = instances.byteLength / instanceSize;
+const cubeVertexCount = cubeVertexData.byteLength / vertexDataSize;
 
-function getInstanceParts(i) {
-  const instance = instances.subarray(i * instanceSize / 4, (i + 1) * instanceSize / 4);
+const cubeInstanceData = new Float32Array([
+  ...makeInstance([0, 0, 0], [0.5, 0.5, 0.5], [0, 0, 0], [1, 1, 1, 1]),
+  ...makeInstance([0, 0, -3], [0.05, 0.05, 0.05], [0, 0, 0], [1, 1, 1, 1]), // light
+]);
+const cubeInstances = cubeInstanceData.byteLength / instanceDataSize;
+
+const planeVertexData = new Float32Array([
+  ...makePlane(10),
+]);
+const planeVertexCount = planeVertexData.byteLength / vertexDataSize;
+
+const planeInstanceData = new Float32Array([
+  ...makeInstance([0, -2, 0], [20, 20, 20], [-Math.PI/2, 0, 0], [1, 1, 1, 1]),
+]);
+const planeInstances = planeInstanceData.byteLength / instanceDataSize;
+
+function getInstanceParts(data, i) {
+  const instance = data.subarray(i * instanceDataSize / 4, (i + 1) * instanceDataSize / 4);
   const tint = instance.subarray(0, 4);
   const model = instance.subarray(4, 20);
   const mvMatrix = instance.subarray(20, 36);
@@ -166,17 +200,29 @@ async function init() {
   });
 
   // 4: Create vertex buffer to contain vertex data
-  const vertexBuffer = device.createBuffer({
-    size: vertices.byteLength, // make it big enough to store vertices in
+  const cubeVertexBuffer = device.createBuffer({
+    size: cubeVertexData.byteLength, // make it big enough to store vertices in
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
   });
-  device.queue.writeBuffer(vertexBuffer, 0, vertices);
+  device.queue.writeBuffer(cubeVertexBuffer, 0, cubeVertexData);
 
-  const instanceBuffer = device.createBuffer({
-    size: instances.byteLength,
+  const cubeInstanceBuffer = device.createBuffer({
+    size: cubeInstanceData.byteLength,
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
   });
-  device.queue.writeBuffer(instanceBuffer, 0, instances);
+  device.queue.writeBuffer(cubeInstanceBuffer, 0, cubeInstanceData);
+
+  const planeVertexBuffer = device.createBuffer({
+    size: planeVertexData.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(planeVertexBuffer, 0, planeVertexData);
+
+  const planeInstanceBuffer = device.createBuffer({
+    size: planeInstanceData.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(planeInstanceBuffer, 0, planeInstanceData);
 
   const lightBuffer = device.createBuffer({
     size: lights.byteLength,
@@ -188,7 +234,7 @@ async function init() {
   const LocInstance = 4;
 
   // 5: Create a GPUVertexBufferLayout and GPURenderPipelineDescriptor to provide a definition of our render pipline
-  const vertexBuffers = [
+  const vertexBufferLayout = [
     {
       attributes: [
         {
@@ -263,7 +309,7 @@ async function init() {
           format: 'float32x4',
         },
       ],
-      arrayStride: instanceSize,
+      arrayStride: instanceDataSize,
       stepMode: 'instance'
     },
   ];
@@ -272,7 +318,7 @@ async function init() {
     vertex: {
       module: shaderModule,
       entryPoint: 'vertex_main',
-      buffers: vertexBuffers
+      buffers: vertexBufferLayout
     },
     fragment: {
       module: shaderModule,
@@ -347,6 +393,26 @@ async function init() {
     );
   }
 
+  let grassTexture;
+  {
+    const response = await fetch('./grass.jpg');
+    const imageBitmap = await createImageBitmap(await response.blob());
+
+    grassTexture = device.createTexture({
+      size: [imageBitmap.width, imageBitmap.height, 1],
+      format: textureFormat,
+      usage:
+        GPUTextureUsage.TEXTURE_BINDING |
+        GPUTextureUsage.COPY_DST |
+        GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+    device.queue.copyExternalImageToTexture(
+      { source: imageBitmap },
+      { texture: grassTexture },
+      [imageBitmap.width, imageBitmap.height]
+    );
+  }
+
   const sampler = device.createSampler({
     magFilter: 'linear',
     minFilter: 'linear',
@@ -382,7 +448,7 @@ async function init() {
     ],
   });
 
-  const textureBindGroup = device.createBindGroup({
+  const cubeTextureBindGroup = device.createBindGroup({
     layout: renderPipeline.getBindGroupLayout(2),
     entries: [
       {
@@ -392,6 +458,20 @@ async function init() {
       {
         binding: 1,
         resource: cubeTexture.createView(),
+      },
+    ],
+  });
+
+  const grassTextureBindGroup = device.createBindGroup({
+    layout: renderPipeline.getBindGroupLayout(2),
+    entries: [
+      {
+        binding: 0,
+        resource: sampler,
+      },
+      {
+        binding: 1,
+        resource: grassTexture.createView(),
       },
     ],
   });
@@ -408,7 +488,7 @@ async function init() {
 
     mat4.identity(viewMatrix);
     mat4.translate(viewMatrix, pos, viewMatrix);
-    // mat4.rotateX(viewMatrix, Math.PI / 4, viewMatrix);
+    mat4.rotateX(viewMatrix, Math.PI / 8, viewMatrix);
     mat4.rotateY(viewMatrix, time, viewMatrix);
   }
 
@@ -417,8 +497,8 @@ async function init() {
   }
 
   function updateInstances(time) {
-    for (let i = 0; i < instanceCount; i++) {
-      const { tint, model, mvMatrix, normalMatrix } = getInstanceParts(i);
+    for (let i = 0; i < cubeInstances; i++) {
+      const { tint, model, mvMatrix, normalMatrix } = getInstanceParts(cubeInstanceData, i);
 
       mat4.identity(mvMatrix);
       mat4.multiply(mvMatrix, viewMatrix, mvMatrix);
@@ -428,6 +508,17 @@ async function init() {
         mat4.rotateY(mvMatrix, time, mvMatrix);
         mat4.rotateX(mvMatrix, time, mvMatrix);
       }
+
+      mat4.invert(mvMatrix, normalMatrix);
+      mat4.transpose(normalMatrix, normalMatrix);
+    }
+
+    for (let i = 0; i < planeInstances; i++) {
+      const { tint, model, mvMatrix, normalMatrix } = getInstanceParts(planeInstanceData, i);
+
+      mat4.identity(mvMatrix);
+      mat4.multiply(mvMatrix, viewMatrix, mvMatrix);
+      mat4.multiply(mvMatrix, model, mvMatrix);
 
       mat4.invert(mvMatrix, normalMatrix);
       mat4.transpose(normalMatrix, normalMatrix);
@@ -445,7 +536,8 @@ async function init() {
     device.queue.writeBuffer(timeBuffer, 0, timeUniform);
     device.queue.writeBuffer(cameraBuffer, 0, camera);
     device.queue.writeBuffer(lightBuffer, 0, lights);
-    device.queue.writeBuffer(instanceBuffer, 0, instances);
+    device.queue.writeBuffer(cubeInstanceBuffer, 0, cubeInstanceData);
+    device.queue.writeBuffer(planeInstanceBuffer, 0, planeInstanceData);
 
     // 7: Create GPUCommandEncoder to issue commands to the GPU
     // Note: render pass descriptor, command encoder, etc. are destroyed after use, fresh one needed for each frame.
@@ -456,7 +548,7 @@ async function init() {
     const renderPassDescriptor = {
       colorAttachments: [
         {
-          clearValue: clearColor,
+          clearValue: { r: 0, g: 0, b: 0, a: 1.0 },
           loadOp: 'clear',
           storeOp: 'store',
           view: context.getCurrentTexture().createView()
@@ -478,10 +570,15 @@ async function init() {
     passEncoder.setPipeline(renderPipeline);
     passEncoder.setBindGroup(0, uniformBindGroup);
     passEncoder.setBindGroup(1, lightsBindGroup);
-    passEncoder.setBindGroup(2, textureBindGroup);
-    passEncoder.setVertexBuffer(0, vertexBuffer);
-    passEncoder.setVertexBuffer(1, instanceBuffer);
-    passEncoder.draw(vertexCount, instanceCount);
+    passEncoder.setBindGroup(2, cubeTextureBindGroup);
+    passEncoder.setVertexBuffer(0, cubeVertexBuffer);
+    passEncoder.setVertexBuffer(1, cubeInstanceBuffer);
+    passEncoder.draw(cubeVertexCount, cubeInstances);
+
+    passEncoder.setBindGroup(2, grassTextureBindGroup);
+    passEncoder.setVertexBuffer(0, planeVertexBuffer);
+    passEncoder.setVertexBuffer(1, planeInstanceBuffer);
+    passEncoder.draw(planeVertexCount, planeInstances);
 
     // End the render pass
     passEncoder.end();
